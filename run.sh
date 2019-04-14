e6f55174b6173cfb2b8dbcd69791c44da2bbbfaf
Necromancer <necromancerr@users.noreply.github.com>
2019-04-14 22:54:59 +0200

    removed debug messages

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 echo "$1" > /tmp/passphrase
 dd of=/tmp/sshkey
-echo "####" 1>&2
-ls -l /tmp/sshkey 1>&2
-echo "####" 1>&2
 puttygen /tmp/sshkey -o /tmp/sshkey.ppk --old-passphrase /tmp/passphrase --new-passphrase /tmp/passphrase
 cat /tmp/sshkey.ppk
 
